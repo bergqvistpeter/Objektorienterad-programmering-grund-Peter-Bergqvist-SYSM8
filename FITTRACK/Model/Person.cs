0892c1f2fcb3192793448219cabf1365209e60f6
bergqvistpeter <peter.bergqvist@edu.newton.se>
2024-10-17 10:33:42 +0200

    Created Person  class, Relay Command and ViewModelBase.

---
+++ FITTRACK/Model/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FITTRACK.Model
+{
+    public abstract class Person
+    {   //Egenskaper
+        public string Username;
+        public string Password;
+        //Konstruktor
+        public Person (string username, string password) 
+        { 
+            this.Username = username;
+            this.Password = password;
+        
+        }
+        //Metod
+        public abstract void SignIn();
+    }
+}
---
+++ FITTRACK/Model/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FITTRACK.Model
+{
+    class User : Person
+    {
+        public string Country;
+        public string SecurityQuestion;
+        public string SecurityAnswer;
+
+        public User(string username, string password) : base(username, password)
+        {
+        }
+
+        public override void SignIn()
+        {
+            if () { }
+        }
+    }
+}
